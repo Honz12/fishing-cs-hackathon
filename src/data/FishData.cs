f7bef5ca22a57f9413e6634ce92eeb2587f52a9d
Honz12 <jparik3@gmail.com>
2026-07-24 10:27:12 +0200

    Add fugu fish

--- src/data/FishData.cs
+++ src/data/FishData.cs
@@ -222,15 +222,15 @@ class FishData
             IsSea = true,
             PricePerKg = 7,
             Image = "mecounObecny.txt"
-        }
+        },
         new TFish() {
-            Name = "Čtverzubec obecný",
+            Name = "Čtverzubec fugu",
             Rarity = FishRarity.Mythic,
             Weight = 2.0,
             WeightVar = 1.5, // 0.5 kg – 3.5 kg
             RodLevel = 9,
             IsSea = true,
-            PricePerKg = 9,
+            PricePerKg = 750,
             Image = "puffer.txt"
         }
     };
