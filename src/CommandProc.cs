d7aa4ac3a80781337ea7968b040c8211ea02ce9f
Honz12 <jparik3@gmail.com>
2026-07-24 11:21:03 +0200

    stupid change

---
+++ src/CommandProc.cs
@@ -0,0 +1,58 @@
+class CommandProc
+{
+    private static PlayerData playerData;
+
+    private static bool running = false;
+
+    private static string helpString = @"Kde Jsou Ryby!? Debug Command Interface
+
+Commands:
+    money <ammount>                 Sets the money of the player.
+    upgrade rod <level>             Sets the upgrade level of the fishing rod.
+    upgrade ship <level>            Sets the upgrade level of the ship.
+    inventory add <name> <weight>   Adds a fish to the inventory.
+";
+
+    public static void Enter(string command, PlayerData playerData)
+    {
+        this.playerData = playerData;
+
+        Console.Clear();
+        Console.WriteLine("Welcome to Kde Jsou Ryby!? Debug Command Interface");
+
+        running = true;
+        Loop();
+    }
+
+    private static void Loop()
+    {
+        while (running)
+        {
+            string? input = Console.ReadLine();
+
+            if (input != null)
+            {
+                ProcessCommand(input);
+            }
+        }
+    }
+
+    private static void ProcessCommand(string cmd)
+    {
+        List<string> parts = new List<string>();
+        string part = "";
+
+        foreach (char c in cmd)
+        {
+            if (c == ' ')
+            {
+                parts.Add(part);
+                part = "";
+            }
+            else
+            {
+                part += c;
+            }
+        }
+    }
+}
