2c83f979c39742fd905db224f3c08c8dc68085fa
Honz12 <jparik3@gmail.com>
2026-07-24 09:07:45 +0200

    Change .NET verison

--- src/data/TFish.cs
+++ src/data/TFish.cs
@@ -8,12 +8,12 @@ enum FishRarity
 
 class TFish // The "TemplateFish".
 {
-    public required string Name;
-    public required double Weight;
-    public required double WeightVar;
-    public required int RodLevel;
-    public required bool IsSea;
-    public required string Image;
+    public string Name;
+    public double Weight;
+    public double WeightVar;
+    public int RodLevel;
+    public bool IsSea;
+    public string Image;
     public FishRarity Rarity;
     public double PricePerKg;
 }
